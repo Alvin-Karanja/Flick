0ccd065d91293c43de3431a2380e486946116659
Alvin-Karanja <alvin2@protonmail.com>
2019-12-05 12:42:17 +0300

    Added utils files

---
+++ app/src/main/java/com/moringa/mymovies/ui/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,9 @@
+package com.moringa.mymovies.ui.util;
+
+public interface ItemTouchHelperAdapter {
+
+    boolean onItemMove(int fromPosition, int toPosition);
+
+
+    void onItemDismiss(int position);
+}
---
+++ app/src/main/java/com/moringa/mymovies/ui/util/OnStartDragListene.java
@@ -0,0 +1,7 @@
+package com.moringa.mymovies.ui.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListene {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
